0f08ea9d023e976c18089ba727a41a1986bae9d0
ItsWachira <brianwachira79@gmail.com>
2022-09-21 14:30:51 +0300

    Updates

---
+++ precache-manifest.0477f3363789ac7cd9a5a4c035bfed51.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050f4016f0bdd85a0ae968f511edc19a",
+    "url": "/MiniGoogleKeeperApp/index.html"
+  },
+  {
+    "revision": "33ad2cf6984f2abce024",
+    "url": "/MiniGoogleKeeperApp/static/js/2.79aacc08.chunk.js"
+  },
+  {
+    "revision": "1faaf656bb875b5c6e78",
+    "url": "/MiniGoogleKeeperApp/static/js/main.1990cdd7.chunk.js"
+  },
+  {
+    "revision": "835b7aac89ef8f2510c7",
+    "url": "/MiniGoogleKeeperApp/static/js/runtime-main.1cc6d82d.js"
+  }
+]);
